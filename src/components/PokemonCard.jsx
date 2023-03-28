aaca637cc6fcdd6c60beef41aae8351077dd88f1
Henno Ambre <henno.ambre@gmail.com>
2023-03-28 12:23:04 +0200

    e

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,11 +1,11 @@
 import PokemonCard from "./components/PokemonCard"
 function PokemonnCard() {
     return (
-    <figure>
-        <img src= "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="pokemon bulbasaur">
-        <figcaption>bulbasaur</figcaption>
-    </figure>
-    );
+        <figure>
+            <img src= "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="pokemon bulbasaur"/> 
+            <figcaption>bulbasaur</figcaption>
+        </figure>
+        );
   }
 
   export default PokemonCard;
