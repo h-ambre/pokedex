0da9da247e4aaefa97bf5a31e4909c2befcdddeb
Henno Ambre <henno.ambre@gmail.com>
2023-03-29 15:17:23 +0200

    react basics 03

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,10 +1,27 @@
 function PokemonCard() {
-    return (
-        <figure>
-            <img src= "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="pokemon bulbasaur"/> 
-            <figcaption>bulbasaur</figcaption>
-        </figure>
-        );
-  }
+  const pokemon = pokemonList[0];
 
-  export default PokemonCard;
+  return (
+    <div>
+      {pokemon.imgSrc !== undefined ? (
+        <img src={pokemon.imgSrc} alt="" />
+      ) : (
+        <p>...</p>
+      )}
+      <p>{pokemon.name}</p>
+    </div>
+  );
+}
+
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
+
+export default PokemonCard;
